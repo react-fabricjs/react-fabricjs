301c2be446a69b8fd0c651d952d5dbd67c62a9ef
huangsiyuan <huangsiyuan@linctex.com>
2023-12-15 13:10:10 +0800

    feat: 添加类型

--- apps/example/src/Fabric.tsx
+++ apps/example/src/Fabric.tsx
@@ -1,15 +1,17 @@
 import { Canvas } from '@react-fabricjs/fiber'
+import { useState } from 'react'
 
 export function Fabric() {
+	const [radius, setRadius] = useState(50)
 	return (
-		<Canvas>
-			<f-circle
-				width={100}
-				height={100}
-				radius={50}
-				backgroundColor="red"
-				borderColor="transparent"
-			/>
-		</Canvas>
+		<>
+			<Canvas>
+				<f-circle width={100} height={100} radius={radius} fill="red" borderColor="transparent" />
+				<f-rect width={100} height={100} fill="blue" />
+			</Canvas>
+			<div>
+				<button onClick={() => setRadius(radius + 10)}>{radius}</button>
+			</div>
+		</>
 	)
 }
--- packages/fiber/src/fabric-types.ts
+++ packages/fiber/src/fabric-types.ts
@@ -7,14 +7,7 @@ export type Properties<T> = Pick<
 export type NonFunctionKeys<T> = { [K in keyof T]-?: T[K] extends Function ? never : K }[keyof T]
 export type Overwrite<T, O> = Omit<T, NonFunctionKeys<O>> & O
 
-/**
- * If **T** contains a constructor, @see ConstructorParameters must be used, otherwise **T**.
- */
-type Args<T> = T extends new (...args: any) => any ? ConstructorParameters<T> : T
-
 export interface NodeProps<T, P> {
-	/** Constructor arguments */
-	args?: Args<P>
 	children?: React.ReactNode
 	ref?: React.Ref<T>
 	key?: React.Key
@@ -25,11 +18,26 @@ export type Node<T, P> = Overwrite<Partial<T>, NodeProps<T, P>>
 
 export type ObjectNode<T, P> = Overwrite<Node<T, P>, {}>
 
-export type ObjectProps = ObjectNode<fabric.Object, typeof fabric.Object>
-export type CircleProps = ObjectNode<fabric.Circle, typeof fabric.Circle>
+export type ObjectProps = ObjectNode<fabric.ICircleOptions, typeof fabric.Object>
+export type CircleProps = ObjectNode<fabric.ICircleOptions, typeof fabric.Circle>
+export type RectProps = ObjectNode<fabric.IRectOptions, typeof fabric.Rect>
+export type TextProps = ObjectNode<fabric.ITextOptions, typeof fabric.Text>
+export type ImageProps = ObjectNode<fabric.IImageOptions, typeof fabric.Image>
+export type PathProps = ObjectNode<fabric.IPathOptions, typeof fabric.Path>
+export type GroupProps = ObjectNode<fabric.IGroupOptions, typeof fabric.Group>
+export type EllipseProps = ObjectNode<fabric.IEllipseOptions, typeof fabric.Ellipse>
+export type LineProps = ObjectNode<fabric.ILineOptions, typeof fabric.Line>
 
 export interface FabricElements {
-	'f-circle': fabric.ICircleOptions
+	'f-circle': CircleProps
+	'f-object': ObjectProps
+	'f-rect': RectProps
+	'f-text': TextProps
+	'f-image': ImageProps
+	'f-path': PathProps
+	'f-group': GroupProps
+	'f-ellipse': EllipseProps
+	'f-line': LineProps
 }
 
 declare global {
