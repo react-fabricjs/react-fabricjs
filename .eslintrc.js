646eb041891a17e50e149c326109018cec30094e
horizon0514 <horizon0514@gmail.com>
2024-01-07 20:17:21 +0800

    remove: delete useless config

--- .eslintrc.js
+++ .eslintrc.js
@@ -90,11 +90,5 @@ module.exports = {
 				'import/no-internal-modules': 'off',
 			},
 		},
-		// {
-		// 	files: ['packages/tldraw/src/test/**/*'],
-		// 	rules: {
-		// 		'import/no-internal-modules': 'off',
-		// 	},
-		// },
 	],
 }
